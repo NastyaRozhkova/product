76203792575262de5caec65c8931785f51057eb9
Vladyslav Batrachenko <vladyslav_batrachenko@epam.com>
2021-07-21 22:42:05 +0300

    add javaDoc for class, implements tests for Copy On Write List and its Iterator

---
+++ src/main/test/com/epam/batrachenko/task2/Iterator/ProductCOWIteratorTest.java
@@ -0,0 +1,44 @@
+package com.epam.batrachenko.task2.Iterator;
+
+import com.epam.batrachenko.task1.Entity.Product;
+import com.epam.batrachenko.task1.List.ProductArrayList;
+import com.epam.batrachenko.task2.List.ProductCOWList;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.Iterator;
+
+public class ProductCOWIteratorTest {
+    @Test
+    public void shouldCorrectlyWorkIteratorInForEach(){
+        ProductCOWList<Product> products=new ProductCOWList<>();
+
+        products.add(0, new Product("test", new BigDecimal("10"), "uk"));
+        products.add(0, new Product("test", new BigDecimal("13"), "uk"));
+
+        ProductCOWList<Product> expectedProducts=new ProductCOWList<>(products);
+        ProductArrayList<Product> newProducts=new ProductArrayList<>();
+        for (Product temp: products) {
+            newProducts.add(temp);
+            products.add(new Product("test", new BigDecimal("13"+temp.getPrice()), "uk"));
+        }
+
+        Assert.assertArrayEquals(expectedProducts.toArray(),newProducts.toArray());
+    }
+
+    @Test
+    public void shouldCorrectlyCopyOnWriteIterator(){
+        ProductCOWList<Product> products=new ProductCOWList<>();
+
+        products.add(0, new Product("test", new BigDecimal("10"), "uk"));
+        products.add(0, new Product("test", new BigDecimal("13"), "uk"));
+
+        Iterator<Product> it= products.iterator();
+
+        products.remove(0);
+        products.remove(0);
+
+        Assert.assertEquals(new Product("test", new BigDecimal("13"), "uk"), it.next());
+    }
+}
---
+++ src/main/test/com/epam/batrachenko/task2/List/ProductCOWListTest.java
@@ -0,0 +1,50 @@
+package com.epam.batrachenko.task2.List;
+
+import com.epam.batrachenko.task1.Entity.Product;
+import com.epam.batrachenko.task1.List.ProductArrayList;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+public class ProductCOWListTest {
+    @Test
+    public void shouldAddProduct() {
+        ProductArrayList<Product> products = new ProductArrayList<>();
+
+        products.add(new Product("test", new BigDecimal("10"), "uk"));
+        products.add(new Product("test", new BigDecimal("11"), "uk"));
+
+        ProductCOWList<Product> productCOWList = new ProductCOWList<>(products);
+        productCOWList.add(new Product("test", new BigDecimal("12"), "uk"));
+
+        Assert.assertEquals(new Product("test", new BigDecimal("12"), "uk"),productCOWList.get(2));
+      }
+
+    @Test
+    public void shouldRemoveProduct() {
+        ProductArrayList<Product> products = new ProductArrayList<>();
+
+        products.add(new Product("test", new BigDecimal("10"), "uk"));
+        products.add(new Product("test", new BigDecimal("11"), "uk"));
+
+        ProductCOWList<Product> productCOWList = new ProductCOWList<>(products);
+        productCOWList.remove(1);
+
+        Assert.assertEquals(productCOWList.size(),1);
+        Assert.assertEquals(productCOWList.get(0),new Product("test", new BigDecimal("10"), "uk"));
+    }
+
+    @Test
+    public void shouldSetProduct() {
+        ProductArrayList<Product> products = new ProductArrayList<>();
+
+        products.add(new Product("test", new BigDecimal("10"), "uk"));
+        products.add(new Product("test", new BigDecimal("11"), "uk"));
+
+        ProductCOWList<Product> productCOWList = new ProductCOWList<>(products);
+        productCOWList.set(1,new Product("test", new BigDecimal("12"), "uk"));
+
+        Assert.assertEquals(new Product("test", new BigDecimal("12"), "uk"),productCOWList.get(1));
+    }
+}
