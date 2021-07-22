3e2602b1f7519f6d12de9467c8692787a7487c45
Vladyslav Batrachenko <vladyslav_batrachenko@epam.com>
2021-07-22 18:36:54 +0300

    refactor implementation

--- src/main/java/com/epam/batrachenko/task2/Iterator/ProductWrapperListIterator.java
+++ src/main/java/com/epam/batrachenko/task2/Iterator/ProductWrapperListIterator.java
@@ -1,34 +1,19 @@
 package com.epam.batrachenko.task2.Iterator;
 
 import com.epam.batrachenko.task1.Entity.Product;
+import com.epam.batrachenko.task1.Iterator.ProductIterator;
 import com.epam.batrachenko.task2.List.ProductWrapperList;
-
-import java.util.Iterator;
-import java.util.NoSuchElementException;
 
 /**
  * An iterator over a collection of products,
  * that consists of unmodifiable and modifiable lists.
- * @see ProductWrapperList , Iterator, Iterable
+ *
  * @author Vladyslav Batrachenko
+ * @see ProductWrapperList , Iterator, Iterable
  */
-public class ProductWrapperListIterator<E extends Product> implements Iterator<E> {
-    private final ProductWrapperList<E> productArrayList;
-    private int index;
+public class ProductWrapperListIterator<E extends Product> extends ProductIterator<E> {
 
     public ProductWrapperListIterator(ProductWrapperList<E> products) {
-        this.productArrayList = products;
-        this.index = -1;
-    }
-
-    public boolean hasNext() {
-        return index < productArrayList.size() - 1;
-    }
-
-    public E next() {
-        if (hasNext()) {
-            return productArrayList.get(++index);
-        }
-        throw new NoSuchElementException();
+        super(products.getList());
     }
 }
--- src/main/java/com/epam/batrachenko/task2/List/ProductCOWList.java
+++ src/main/java/com/epam/batrachenko/task2/List/ProductCOWList.java
@@ -16,82 +16,16 @@ import java.util.List;
  */
 public class ProductCOWList<E extends Product> extends ProductArrayList<E> implements List<E> {
 
+    @Override
+    public Iterator<E> iterator() {
+        return new ProductCOWIterator<>(new ProductCOWList<>(this));
+    }
+
     public ProductCOWList() {
     }
 
     public ProductCOWList(ProductArrayList<E> products) {
         super(products);
-        this.array = (E[]) new Product[products.size()];
-        this.size = products.size();
-        System.arraycopy(products.toArray(array), 0, this.array, 0, this.size);
-    }
-
-    @Override
-    public Iterator<E> iterator() {
-        return new ProductCOWIterator<>(this);
-    }
-
-    @Override
-    public void clear() {
-        E[] products = (E[]) new Product[DEFAULT_CAPACITY];
-        int sizeCopy = 0;
-        setArray(products, sizeCopy);
-    }
-
-    @Override
-    public E set(int index, E element) {
-        E[] products = getArray();
-        int sizeCopy = this.size;
-        products[index] = element;
-        setArray(products, sizeCopy);
-        return products[index];
-    }
-
-    @Override
-    public void add(int index, E element) {
-        E[] products = getArray();
-        int sizeCopy = this.size;
-        if (products.length == sizeCopy) {
-            E[] newArray = (E[]) new Product[products.length + 1];
-            System.arraycopy(products, 0, newArray, 0, index);
-            System.arraycopy(products, index, newArray, index + 1, sizeCopy - index);
-            products = newArray;
-        } else {
-            System.arraycopy(products, index, products, index + 1, sizeCopy - index);
-        }
-        products[index] = element;
-        ++sizeCopy;
-        setArray(products, sizeCopy);
-    }
-
-    @Override
-    public E remove(int index) {
-        E[] products = getArray();
-        int sizeCopy = this.size;
-        E temp = products[index];
-        if (index + 1 == sizeCopy) {
-            products[index] = null;
-        } else {
-            System.arraycopy(products, index + 1, products, index, sizeCopy - index);
-        }
-        --sizeCopy;
-        setArray(products, sizeCopy);
-        return temp;
-    }
-
-    @Override
-    public boolean add(E e) {
-        this.add(this.size, e);
-        return true;
-    }
-
-    private E[] getArray() {
-        return this.array;
-    }
-
-    private void setArray(E[] a, int size) {
-        array = a;
-        this.size = size;
     }
 }
 
